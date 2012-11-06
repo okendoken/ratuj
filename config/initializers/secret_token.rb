2258e1f7c3d8d5047aa5a8ae88bd4cc83c079264
oken <philip.daineka@gmail.com>
2012-11-05 22:06:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratuj::Application.config.secret_token = '210502d23649c62868c92df143f820a0934b3b954aea6aad45003264a291c7c3050f266960c30d0fbf8fd9980df31c6d214a4c89fd18422de89e016b013cc98e'
